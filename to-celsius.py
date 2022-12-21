dc6f15298546afdb85e51b404e005accb7525286
mrfstop <jbrooks1040@gmail.com>
2022-12-21 11:07:43 -0500

    First commit

---
+++ to-celsius.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.7
+
+# Python implementation goes here
+
+fahrenheit = float(input("What temperature, in Fahrenheit, would you like converted to Celsius? "))
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit, "F is", celsius, "C")
